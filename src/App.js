f220d5ab803bb7208cebd11820accf02a2d6fb3d
Mike Nicklas <mike.nicklas@gmail.com>
2018-05-22 20:24:16 -0400

    Simple implementation of react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Lander from './Lander';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <h1>HelpAroundHere</h1>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/opportunities">Volunteering Opportunities</Link></li>
+            <li><Link to="/add-opportunity">Add an Opportunity</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" render={() => <Lander message="viewing home" />} />
+          <Route path="/opportunities" render={() => <Lander message="viewing /opportunities" />} />
+          <Route path="/add-opportunity" render={() => <Lander message="viewing /add-opportunity" />} />
         </div>
-      </div>
+      </Router>
     );
   }
 }
